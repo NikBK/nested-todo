b96037bd0a75bb25934d3a8e167c38ce4c40839f
NikBK <nikhilbalakrishna5@gmail.com>
2023-06-14 00:06:01 +0530

    update frontend url

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(
     cors({
-        origin: ["https://nested-todo-nikbk.vercel.app"],
+        origin: ["https://nested-todo-nikbk.vercel.app/"],
         methods: ["GET", "POST"],
         credentials: true,
     })
